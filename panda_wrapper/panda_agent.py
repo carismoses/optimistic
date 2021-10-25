6df76465938914c7524694f1009e8dc1ba3464af
Caris M Moses <caris.moses@gmail.com>
2021-10-25 17:10:35 -0400

    can now execute actions with simulated panda

--- panda_wrapper/panda_agent.py
+++ panda_wrapper/panda_agent.py
@@ -7,63 +7,55 @@ import time
 import pb_robot
 
 class PandaAgent:
-    def __init__(self):
+    def __init__(self, vis):
         """
         Build the Panda world in PyBullet using pb_robot. This matches the set up
         of George the Panda in the LIS lab
         """
+        self.real = False # TODO: update to command line arg when use real robot
         robot_pose = np.eye(4)
         # Setup PyBullet instance to run in the background and handle planning/collision checking.
         self._planning_client_id = pb_robot.utils.connect(use_gui=False)
         self.plan()
         pb_robot.utils.set_default_camera()
+        pb_robot.utils.set_camera(90, -15, 1.5)
         self.planning_robot = pb_robot.panda.Panda()
         self.planning_robot.arm.hand.Open()
-        self.planning_robot.set_transform(robot_pose)
+        self.table = self.add_table()
+        self.planning_robot.set_transform(np.eye(4))
         self.orig_joint_angles = self.planning_robot.arm.GetJointValues()
-        self.fixed = self.add_fixed_objects()
+
 
         # Setup PyBullet instance that only visualizes plan execution.
         # State needs to match the planning instance.
-        self._execution_client_id = pb_robot.utils.connect(use_gui=True)
+        self._execution_client_id = pb_robot.utils.connect(use_gui=vis)
         self.execute()
         pb_robot.utils.set_default_camera()
+        pb_robot.utils.set_camera(90, -15, 1.5)
         self.execution_robot = pb_robot.panda.Panda()
         self.execution_robot.arm.hand.Open()
-        self.execution_robot.set_transform(robot_pose)
-        self.add_fixed_objects()
+        self.execution_robot.set_transform(np.eye(4))
+        self.add_table()
 
         self.plan()
 
 
-    def add_fixed_objects(self):
-        fixed_objects = []
-        # add table, frame, and collision walls to model
-        table_x_offset = 0.2
-        urdfs_and_points = [('panda_table.urdf', [table_x_offset,
-                                                    0,
-                                                    0]),
-                            ('panda_frame.urdf', [table_x_offset + 0.762 - 0.0127,
-                                                    0 + 0.6096 - 0.0127,
-                                                    0]),
-                            ('walls.urdf', [table_x_offset + 0.762 + 0.005,
-                                                    0,
-                                                    0])]
+    def add_table(self):
         path_prefix = 'panda_wrapper/urdf_models'
         pb_robot_prefix = 'pb_robot/models'
-        for urdf, point in urdfs_and_points:
-            shutil.copyfile(os.path.join(path_prefix, urdf),
-                            os.path.join(pb_robot_prefix, urdf))
-            file = os.path.join('models', urdf)
-            pb_object = pb_robot.body.createBody(file)
-            pb_object.set_point(point)
-            fixed_objects.append(pb_object)
-        # HACK
-        self.table = fixed_objects[0]
-        return fixed_objects
+        # add table
+        table_x_offset = 0.2
+        table_urdf = 'panda_table.urdf'
+        table_point = [table_x_offset, 0, 0]
+        shutil.copyfile(os.path.join(path_prefix, table_urdf),
+                        os.path.join(pb_robot_prefix, table_urdf))
+        file = os.path.join('models', table_urdf)
+        table = pb_robot.body.createBody(file)
+        table.set_point(table_point)
+        return table
 
 
-    def _add_text(self, txt):
+    def add_text(self, txt):
         self.execute()
         pb_robot.viz.remove_all_debug()
         self.txt_id = pb_robot.viz.add_text(txt, position=(0, 0.25, 0.75), size=2)
@@ -105,8 +97,10 @@ class PandaAgent:
         print("Resetting world")
         self.plan()
         self.planning_robot.arm.SetJointValues(self.orig_joint_angles)
+        self.planning_robot.arm.hand.Open()
         self.execute()
         self.execution_robot.arm.SetJointValues(self.orig_joint_angles)
+        self.execution_robot.arm.hand.Open()
         print("Done")
 
 
@@ -115,18 +109,49 @@ class PandaAgent:
         Get the PDDL representation of the robot and table.
         """
         conf = pb_robot.vobj.BodyConf(self.planning_robot, self.planning_robot.arm.GetJointValues())
-        print('Initial configuration:', conf.configuration)
+        table_pose = pb_robot.vobj.BodyPose(self.table, self.table.get_base_link_pose())
         init = [('conf', conf),
                 ('atconf', conf),
-                ('handempty',)]
+                ('handempty',),
+                ('pose', self.table, table_pose),
+                ('atpose', self.table, table_pose),
+                ('table', self.table)]
         return init
 
-    def step_simulation(self):
-        print('Press Ctrl-C to resume execution.')
-        try:
-            while True:
-                p.stepSimulation(physicsClientId=self._execution_client_id)
-                p.stepSimulation(physicsClientId=self._planning_client_id)
-                time.sleep(1/2400.)
-        except KeyboardInterrupt:
-            pass
+
+    def execute_action(self, action, fixed, world_obstacles=[], pause=True, sim_fatal_failure_prob=0.0, sim_recoverable_failure_prob=0.0):
+        obstacles = world_obstacles + fixed
+        name, args = action
+        executionItems = args[-1]
+        self.execute()
+        for e in executionItems:
+            if self.real:
+                e.simulate(timestep=0.1, obstacles=obstacles)
+            else:
+                e.simulate(timestep=0.5, obstacles=obstacles)
+
+            # Simulate failures if specified
+            if (name in ["pick", "move_free"] and not isinstance(e, pb_robot.vobj.BodyGrasp)
+                and not isinstance(e, pb_robot.vobj.MoveFromTouch)):
+                if np.random.rand() < sim_fatal_failure_prob:
+                    raise ExecutionFailure(fatal=True,
+                        reason=f"Simulated fatal failure in {e}")
+                elif np.random.rand() < sim_recoverable_failure_prob:
+                    raise ExecutionFailure(fatal=False,
+                        reason=f"Simulated recoverable failure in {e}")
+            if pause:
+                time.sleep(0.1)
+
+        if self.real:
+            input("Execute on Robot?")
+            try:
+                from franka_interface import ArmInterface
+            except:
+                print("Do not have rospy and franka_interface installed.")
+                return
+            arm = ArmInterface()
+            arm.set_joint_position_speed(0.3)
+            print("Executing on real robot")
+            input("start?")
+            for e in executionItems:
+                e.execute(realRobot=arm, obstacles=obstacles)
