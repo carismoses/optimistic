164f50b0be01b6ea6424352e915be12f2951e0ad
Caris M Moses <caris.moses@gmail.com>
2021-11-18 11:50:27 -0500

    removing more old heuristics code

--- learning/datasets.py
+++ learning/datasets.py
@@ -111,30 +111,6 @@ class TransDataset(Dataset):
             n_min += 1
         print('Done balancing dataset.')
 
-class HeurDataset(Dataset):
-    def __init__(self):
-        self.object_features = torch.tensor([], dtype=torch.float64)
-        self.edge_features = torch.tensor([], dtype=torch.float64)
-        self.goal_edge_features = torch.tensor([], dtype=torch.float64)
-        self.steps_to_goal = torch.tensor([], dtype=torch.float64)
-
-    def __getitem__(self, ix):
-        return [self.object_features[ix],
-                self.edge_features[ix],
-                self.goal_edge_features[ix]], \
-                self.steps_to_goal[ix]
-
-    def __len__(self):
-        """
-        The total number of datapoints in the entire dataset.
-        """
-        return len(self.object_features)
-
-    def add_to_dataset(self, object_features, edge_features, goal_edge_features, steps_to_goal):
-        self.object_features = torch.cat([self.object_features, torch.tensor([object_features])])
-        self.edge_features = torch.cat([self.edge_features, torch.tensor([edge_features])])
-        self.goal_edge_features = torch.cat([self.goal_edge_features, torch.tensor([goal_edge_features])])
-        self.steps_to_goal = torch.cat([self.steps_to_goal, torch.tensor([steps_to_goal])])
 
 # this is only for single inputs (not batches)
 # TODO: have it detect if a single input or a batch is being passed in
