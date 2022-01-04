c972500cd1654a2393a4f40a31dec9b28d6b2e52
Caris M Moses <caris.moses@gmail.com>
2022-01-04 14:26:40 -0500

    adding docker files

---
+++ docker/random_goals/random_goals.sh
@@ -0,0 +1,20 @@
+# configure minio
+#./mc config host add honda_cmm $CSAIL_ENDPOINT $HONDA_ACCESS $HONDA_SECRET
+
+# clone optimistic repo
+git clone https://github.com/carismoses/optimistic.git
+
+# link other packages
+cd optimistic
+ln -s /pb_robot/src/pb_robot .
+ln -s /pddlstream/pddlstream .
+
+# run training code
+python3 -m experiments.collect_data_and_train \
+              --domain tools \
+              --exp-name random_goals \
+              --data-collection-mode random-goals \
+              --n-models 1
+
+# copy over results to minio
+#/./mc cp -r learning/experiments/logs/ honda_cmm/stacking/rss_camera_ready/
