10f1235e8d0a82ee10e7bed9de01c011eb56e523
Caris Moses <caris.moses@gmail.com>
2022-01-11 09:37:06 -0500

    update dockerfiles (can't get minio to work)

--- docker/restart_experiments/sequential_goals/restart_sequential_goals.sh
+++ docker/restart_experiments/sequential_goals/restart_sequential_goals.sh
@@ -1,12 +1,11 @@
 # configure minio
-./mc config host add honda_cmm https://ceph.csail.mit.edu 03ba63de07cf423daa1bb0e4ecca35c9 bb6116aa4549415fb1c85fc2f03ffd34
+#./mc config host add honda_cmm https://ceph.csail.mit.edu 03ba63de07cf423daa1bb0e4ecca35c9 bb6116aa4549415fb1c85fc2f03ffd34
 
 # clone optimistic repo
 git clone https://github.com/carismoses/optimistic.git
 
 # move experiment to right path
-mkdir -p optimistic/logs/experiments
-mv sequential_goals-20220105-145239 optimistic/logs/experiments
+mv sequential_goals-20220105-143004 optimistic/logs/experiments
 
 # link other packages
 cd optimistic
@@ -16,7 +15,7 @@ ln -s /pddlstream/pddlstream .
 # run training code
 python3 -m experiments.collect_data_and_train \
               --restart \
-              --exp-path
+              --exp-path logs/experiments/sequential_goals-20220105-143004
 
 # copy over results to minio
 #/./mc cp -r learning/experiments/logs/ honda_cmm/stacking/rss_camera_ready/
