d960556857feef04c372b93639a24072756271c9
Caris M Moses <caris.moses@gmail.com>
2022-01-20 14:47:48 -0500

    adding file to calc bald scores

---
+++ evaluate/calc_bald_scores.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+
+from learning.utils import ExperimentLogger
+from experiments.strategies import bald
+from learning.utils import model_forward
+from domains.utils import init_world
+
+##
+dataset_paths = {'0': ['logs/experiments/sequential_goals-20220120-030537'],
+'1':['logs/experiments/sequential_goals-20220120-030624'],
+'2':['logs/experiments/sequential_goals-20220120-030629'],
+'3':['logs/experiments/sequential_goals-20220120-030635'],
+'4':['logs/experiments/sequential_goals-20220120-030640']}
+##
+
+#fig, ax = plt.subplots()
+
+import pdb; pdb.set_trace()
+cs = ['r', 'g', 'b', 'c', 'm', 'y', 'k']
+for ei, (exp_name, exp_path) in enumerate(dataset_paths.items()):
+    exp_path = exp_path[0]
+    bald_scores = []
+    logger = ExperimentLogger(exp_path)
+    world = init_world('tools', None, 'optimistic', False, logger)
+    model = logger.load_trans_model(world, i=0)
+    next_model_i = 12
+    xs = []
+    for dataset, di in logger.get_dataset_iterator():
+        if di > next_model_i:
+            model = logger.load_trans_model(world, i=next_model_i)
+            next_model_i += 12
+        if len(dataset) > 0:
+            (of, ef, af), l = dataset[-1]
+            predictions = model_forward(model, [of, ef, af], single_batch=True)
+            mean_prediction = predictions.mean()
+            score = mean_prediction*bald(predictions)
+            bald_scores.append(score)
+            xs.append(di)
+    plt.plot(xs, bald_scores, color=cs[ei], label=exp_name)
+plt.legend()
+plt.xlabel('Num Actions')
+plt.ylabel('BALD Score')
+plt.title('BALD Scores')
+#plt.show()
+plt.savefig('bald_scores.png')
