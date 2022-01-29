4a729ec17ca895e2690eb01a642673654d5715f7
Caris M Moses <caris.moses@gmail.com>
2022-01-29 12:48:47 -0500

    updating make dataset docker scripts

---
+++ docker/make_datasets/make_datasets.sh
@@ -0,0 +1,15 @@
+# clone optimistic repo
+git clone https://github.com/carismoses/optimistic.git
+
+# link other packages
+cd optimistic
+ln -s /pb_robot/src/pb_robot .
+ln -s /pddlstream/pddlstream .
+
+# run training code
+python3 -m evaluate.gen_dataset \
+            --max-actions N_ACTIONS \
+            --balanced BALANCED \
+            --exp-name EXP_NAME \
+            --goal-progress GP \
+            --n-datasets N_DATASETS
