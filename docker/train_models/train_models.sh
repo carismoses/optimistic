dc9a643cc09691873fbf069a4c3a02a711a23910
Caris Moses <caris.moses@gmail.com>
2022-01-30 15:24:25 -0500

    fix docker for training

--- docker/train_models/train_models.sh
+++ docker/train_models/train_models.sh
@@ -7,7 +7,16 @@ ln -s /pb_robot/src/pb_robot .
 ln -s /pddlstream/pddlstream .
 
 # run training code
+cp -r /$PATH1 logs/experiments/
+cp -r /$PATH2 logs/experiments/
+cp -r /$PATH3 logs/experiments/
+cp -r /$PATH4 logs/experiments/
+cp -r /$PATH5 logs/experiments/
 
 python3 -m experiments.train_from_data \
-            --dataset-exp-path $PATH1 $PATH2 $PATH3 $PATH4 $PATH5 \
+            --dataset-exp-path logs/experiments/$PATH1 \
+                               logs/experiments/$PATH2 \
+                               logs/experiments/$PATH3 \
+                               logs/experiments/$PATH4 \
+                               logs/experiments/$PATH5 \
             --train-freq $TRAIN_FREQ
