1bccf838c2e0bcf05d6248c81a2dce830bc2aee5
Caris Moses <caris.moses@gmail.com>
2022-03-08 17:16:29 -0500

    can restart gen search samples

--- experiments/gen_search_space.py
+++ experiments/gen_search_space.py
@@ -10,13 +10,14 @@ from tamp.utils import get_simple_state
 
 
 ## Parameters ##
-n_plans = 2       # number of plans per contact type
+n_plans = 300       # number of plans per contact type
 save_path = 'logs/search_space_samples.pkl'
 fig_dir = 'logs/search_space_figs/'
 goal_obj='yellow_block'
 goal_type='push'
 contact_types=['push_pull', 'poke']
-import pdb; pdb.set_trace()
+restart = True
+#import pdb; pdb.set_trace()
 ##
 
 world = ToolsWorld(False, None, contact_types=contact_types)
@@ -28,10 +29,22 @@ for contact in contacts:
         contact_preds[contact[0].type] = contact[0]
 world.disconnect()
 
-all_plans = []
+counts = {ctype: 0 for ctype in contact_types}
+type_plans = {ctype: [] for ctype in contact_types}
+if restart:
+    with open(save_path, 'rb') as handle:
+        all_plans = pickle.load(handle)
+    for plan_info in all_plans:
+        ctype = plan_info[0][-1].args[5].type
+        counts[ctype] += 1
+        type_plans[ctype] += [plan_info]
+else:
+    all_plans = []
+
+print(counts)
+
 for ctype in contact_types:
-    type_plans = []
-    pi = 0
+    pi = counts[ctype]
     while pi < n_plans:
         world = ToolsWorld(False, None, contact_types=[ctype])
         goal, add_to_state = world.generate_goal(goal_obj, goal_type)
@@ -43,16 +56,17 @@ for ctype in contact_types:
             problem = problem[:3] + problem[4:] # remove stream map
             plan_info = (pddl_plan, problem, init_expanded)
             all_plans.append(plan_info)
-            type_plans.append(plan_info)
+            type_plans[ctype].append(plan_info)
             with open(save_path, 'wb') as handle:
                 pickle.dump(all_plans, handle)
 
             # visualize all samples
             fig, ax = plt.subplots()
-            for plan,_,_ in type_plans:
+            for plan,_,_ in type_plans[ctype]:
                 goal_xy = world.action_to_vec(plan[-1])
                 ax.plot(*goal_xy, 'k.')
             world.vis_tool_ax(contact_preds[ctype], ax, frame='cont')
             plt.savefig(fig_dir+ctype+'.png')
             plt.close()
         world.disconnect()
+
