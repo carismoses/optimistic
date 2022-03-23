36b226e6830f76f13660429ca790b154b8100ce7
Caris M Moses <caris.moses@gmail.com>
2022-03-23 12:06:47 -0400

    update skel plotting code

--- evaluate/vis_skeletons.py
+++ evaluate/vis_skeletons.py
@@ -9,47 +9,70 @@ from domains.tools.world import ToolsWorld
 from experiments.utils import ExperimentLogger
 from evaluate.plot_value_fns import get_model_accuracy_fn, get_seq_fn
 from domains.tools.primitives import get_contact_gen
+from experiments.skeletons import get_skeleton_fns
 
 
 dir = 'skeletons'
+
 
 def get_plan_name(plan):
     plan_name = ''
     for name, args in plan:
         if name not in ['move_holding', 'move_free']:
             if name == 'move_contact':
-                plan_name += name+'_'
                 obj = args[2].readableName
+                if plan_name == '':
+                    plan_name += obj+'_'
+                plan_name += name+'_'
                 ctype = args[5].type
-                plan_name += obj+'_'
                 plan_name += ctype+'_'
             elif name == 'pick' and args[0].readableName != 'tool':
-                plan_name += name+'_'
                 obj = args[0].readableName
-                plan_name += obj+'_'
+                if plan_name == '':
+                    plan_name += obj+'_'
+                plan_name += name+'_'
     return plan_name[:-1]
 
 
 def gen_plots(args):
     logger = ExperimentLogger(args.dataset_exp_path)
+
+    # use skeleton fns instead of plans in logger so always in same order (and
+    # use the same colors for comparing plots)
     all_plans = []
-    for plan, pi in logger.get_plan_iterator():
-        plan_name = get_plan_name(plan)
-        if plan_name not in all_plans:
-            all_plans.append(plan_name)
+    for skel_fn in get_skeleton_fns():
+        dummy_world = ToolsWorld()
+        dummy_goal = dummy_world.generate_dummy_goal()
+        skel = skel_fn(dummy_world, dummy_goal)
+        for obj in ['yellow_block', 'blue_block']:
+            new_plans = [obj]
+            for name, skel_args in skel:
+                if name == 'move_contact':
+                    old_plans = new_plans
+                    new_plans = []
+                    for old_plan in old_plans:
+                        for ctype in ['push_pull', 'poke']:
+                            new_plans.append(old_plan+'_move_contact_'+ctype)
+                elif name == 'pick' and skel_args[0].readableName != 'tool':
+                    old_plans = new_plans
+                    new_plans = []
+                    for old_plan in old_plans:
+                        new_plans.append(old_plan+'_pick')
+            all_plans += new_plans
+
 
     skel_count = {pn: [0] for pn in all_plans}
     for plan, pi in logger.get_plan_iterator():
         plan_name = get_plan_name(plan)
         for pn in all_plans:
             if plan_name == pn:
-
+                #print('matching name!')
                 skel_count[pn] += [1]
             else:
                 skel_count[pn] += [0]
 
     fig, ax = plt.subplots()
-    print(skel_count.keys())
+    #print(skel_count.keys())
     for plan_name, counts in skel_count.items():
         ax.bar(np.arange(len(counts)), counts, width=1.0, label=plan_name)
     ax.legend()
